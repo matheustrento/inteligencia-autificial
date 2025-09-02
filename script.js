546e19367b91f045162e1b7e059ae03a5efc72c0
matheustrento <159184960+matheustrento@users.noreply.github.com>
2025-09-02 11:18:28 -0300

    priojetim pa nois

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-prguntas");
+const caixaAlternativas = document.querySelector(".caixa-altrnativas");
+const caixaResultado = document.querySelector(".caixa-reultado");
+const caixaResultado  = document.querySelector(".caixa-reultado");
