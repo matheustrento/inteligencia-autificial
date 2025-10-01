3a1e97aa16c2d5a8554b218bf0151bdcc7d41f75
matheustrento <159184960+matheustrento@users.noreply.github.com>
2025-10-01 12:44:04 -0300

    projeto

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const perguntas = [
     alternativas:[
         {
         texto:"praticar esportes",
-        afirmacao:"afirmação"
+        afirmacao:"parabens pelo visto voce gosta de se movimentar "
         },
         {
        texto:"jogar jogos online",
-       afirmacao:"afirmação"
+       afirmacao:"bom voce nao gosta muito de se movimentar mais pode estar aprendendo algo com jogos"
         }
 
     ]
@@ -22,14 +22,14 @@ const perguntas = [
 
 {
     
-        enunciado: "esportes x jogos ",
+        enunciado: "mas quais as consequencias de cada um ? ",
         alternativas:[{
             texto:"a pratica de esportes pode ser benefica a saude.",
-            afirmacao:"afirmação"
+            afirmacao:"a pratica de esportes pode ser benefica a saude, e ate mesmo a saude mental"
         },
         {        
-           texto:"jogos onlines podem nao ser muito bons a saude e a sosializacao.",
-           afirmacao:"afirmação"
+           texto:"Jogar jogos onlines com frequencia podem nao ser muito bom a saude.",
+           afirmacao:"Jogar jogos onlines com frequencia podem nao ser muito bom a saude, principalmente pela sosializacao"
         }        
         ]
     },
@@ -37,45 +37,45 @@ const perguntas = [
 
     {
     
-        enunciado: "mas nen todos os esportes podem ser sempre beneficos, como tambem nem todos os jogos podem ser maleficos?",
+        enunciado: "mas nem todos os esportes podem ser sempre beneficos, como tambem nem todos os jogos podem ser maleficos?",
         alternativas:[
             {
             texto:"pois a pratica pode melhorar o desempenho, mas tem o risco de lesoes.",
-            afirmacao:"afirmação"
+            afirmacao:"pois a pratica pode melhorar o desempenho, mas tem o risco de lesoes, mas por isso devem ser praticados com cuidado"
             },
             {
             texto:"existem diversos maleficios, mas posuem jogos que ajudam a melhorar o raciocinio.",
-            afirmacao:"afirmação"
+            afirmacao:" mas posuem jogos que ajudam a melhorar o raciocinio, por isso devemos ecolher bem os jogos "
             }
         ]
     },
     
     {
     
-        enunciado: "tambem voce prefere em questao a esses temas ?",
+        enunciado: "quanto aos temas voce prefere?",
         alternativas:[
             {
-            texto:"praticar com os com os amigos.",
-            afirmacao:"afirmação"
+            texto:"os realizar sozinhos.",
+            afirmacao:"os realizar sozinhos, bom talvez isso o acabe prejudicando no futuro"
         },
         {
-            texto:"jogar com os amigos.",
-            afirmacao:"afirmação"
+            texto:"os realizar sosializando ou realizando com mais pessoas.",
+            afirmacao:"aos realizar com mais pessoas, podem aver ate mesmo uma troca de expriencias"
         }
         ]
     },
 
     {
     
-        enunciado: "mas qual deses temas mais reunen os amigos?",
+        enunciado: "mas qual voce tem mais acesso?",
         alternativas:[
             {
-            texto:"os esportes praticados com mais pessoas ou um grupo pode ser muito bom,alem da troca e esperiencias.",
-            afirmacao:"afirmação"
+            texto:" esportes.",
+            afirmacao:"que bom, pois o assesso a esportes acaba se tornando mais dificil"
             },
         {
-            texto:"jogar com os amigos tambem e uma pratica de lazer, ainda se for um jogo de raciocinio logico.",
-            afirmacao:"afirmação"
+            texto:"jogos.",
+            afirmacao:"hoje a maioria tem mais acesso aos jogos, por isso se puder pensar bem nas ecolhas ajudara voce mesmo"
         }
         ]
     },
@@ -115,9 +115,9 @@ function mostraPergunta(){
 
 
      function mostraResultado(){
-        caixaPerguntas.textContent = "Em 2049...";
+        caixaPerguntas.textContent = "Em 2025...";
         textoResultado. textContent = historiaFinal;
-        caixaAlternativas.textContent = "";
+        caixaAlternativas.textContent = "A principal diferença é que esporte é uma atividade física institucionalizada com regras rígidas, enquanto jogo é uma atividade recreativa cujas regras podem ser adaptadas pelos próprios participantes. Os "jogos online", no contexto de competições profissionais e estruturadas, são conhecidos como eSports (ou esportes eletrônicos), onde jogadores com treinamento e apoio profissional competem em um nível competitivo semelhante ao dos esportes tradicionais, mas através de plataformas digitais. ";
      }
     
      mostraPergunta();
